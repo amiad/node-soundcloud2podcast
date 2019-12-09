26ca4e7d4d8a77c8caa1f7611bf98f7425033962
Amiad <hatul@hatul.info>
2019-12-09 11:20:03 +0200

    add auto update to clientId

--- Soundcloud2Podcast.js
+++ Soundcloud2Podcast.js
@@ -6,7 +6,10 @@ let rss = require('rss');
 let mime = require('mime-types')
 let express = require('express');
 
-const CLIENT_ID = "UW9ajvMgVdMMW3cdeBi8lPfN6dvOVGji";
+const CLIENT_ID_PATH = 'client_id';
+const YOUTUBE_DL_CLIENT_ID_URL = 'https://raw.githubusercontent.com/ytdl-org/youtube-dl/master/youtube_dl/extractor/soundcloud.py';
+
+let clientId;
 
 module.exports = class Soundcloud2Podcast {
 
@@ -42,21 +45,37 @@ module.exports = class Soundcloud2Podcast {
 		return url;
 	}
 
-	get_soundcloud_json(url){
-		let api_url = "https://api.soundcloud.com/resolve.json?client_id=" + CLIENT_ID + "&url=" + url;
-		let res = request('GET', api_url);
-		let json;
-		if (res.statusCode != 200)
-			this.die('request to soundcloud failed!');
+	getSoundcloudJson(url){
+		let clientId = this.getLocalClientId();
+		let res = this.getSoundcloudApi(url, clientId, false);
 
+		if (!res){
+			this.saveRemoteClientId();
+			res = this.getSoundcloudApi(url, true);
+		}
+
+		let json;
 		try {
 			json = JSON.parse(res.body);
 		}
 		catch(err){
+			console.error(err);
 			this.die('json file is wrong!');
 		}
-
 		return json;
+	}
+
+	getSoundcloudApi(url, dieIfFailed){
+		let apiUrl = "https://api.soundcloud.com/resolve.json?client_id=" + this.clientId + "&url=" + url;
+		let res = request('GET', apiUrl);
+		if (res.statusCode != 200){
+			 if (dieIfFailed)
+				this.die('request to soundcloud failed!');
+		else
+			return false;
+		}
+
+		return res;
 	}
 
 	get_feed(){
@@ -65,9 +84,9 @@ module.exports = class Soundcloud2Podcast {
 		if (cache)
 			return cache;
 
-		let sc = this.get_soundcloud_json(url);
+		let sc = this.getSoundcloudJson(url);
 		if (sc.kind != 'user')
-			this.user = this.get_soundcloud_json(sc.user.permalink_url);
+			this.user = this.getSoundcloudJson(sc.user.permalink_url);
 		else
 			this.user = sc;
 
@@ -76,7 +95,7 @@ module.exports = class Soundcloud2Podcast {
 
 	generate_feed(sc){
 		let feed = this.generate_basic_feed(sc);
-		let tracks = sc.kind == 'user' ? this.get_soundcloud_json(sc.permalink_url + "/tracks") : sc.tracks;
+		let tracks = sc.kind == 'user' ? this.getSoundcloudJson(sc.permalink_url + "/tracks") : sc.tracks;
 
 		feed = this.add_items_to_feed(feed, tracks);
 
@@ -112,9 +131,9 @@ module.exports = class Soundcloud2Podcast {
 				description: track.description,
 				url: track.permalink_url,
 				guid: track.permalink_url,
-				pubDate: track.created_at,
+				date: track.created_at,
 				enclosure: {
-					url: download_url + '?client_id=' + CLIENT_ID,
+					url: download_url + '?client_id=' + this.clientId,
 					size: track.original_content_size,
 					type: mime.lookup(track.original_format)
 				},
@@ -158,6 +177,30 @@ module.exports = class Soundcloud2Podcast {
 			options.custom_namespaces = {'itunes': 'http://www.itunes.com/dtds/podcast-1.0.dtd'};
 			options.custom_elements = [{'itunes:image': {_attr: {href: image_url}}}];
 		}
+	}
+
+	getLocalClientId(){
+		if (!fs.existsSync(CLIENT_ID_PATH))
+			return this.saveRemoteClientId();
+
+		this.clientId = fs.readFileSync(CLIENT_ID_PATH);
+		return this.clientId;
+	}
+
+	saveRemoteClientId(){
+		let res = request('GET', YOUTUBE_DL_CLIENT_ID_URL);
+		if (res.statusCode != 200)
+			this.die('request to youtube-dl failed!');
+
+		let pattern = /_CLIENT_ID = \'([a-zA-Z0-9]*)\'/;
+		let matches = res.body.toString().match(pattern);
+
+		if (!matches[1])
+			die('clientid not found!');
+			
+		fs.writeFile(CLIENT_ID_PATH, matches[1], () => {});
+		this.clientId = matches[1];
+		return this.clientId;
 	}
 
 	die(message){
